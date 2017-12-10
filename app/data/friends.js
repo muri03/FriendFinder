3c6fdfeb1e09241e3616c4749678b5bf048c0e0b
Muriel <muriel.mangubat@gmail.com>
2017-12-10 07:05:59 -0600

    restored files

---
+++ app/data/friends.js
@@ -0,0 +1,49 @@
+{
+  "name":"Liam",
+  "photo":"https://vignette.wikia.nocookie.net/despicableme/images/7/7f/Minion-Bob-With-Teddy-Bear.jpg/revision/latest/scale-to-width-down/480?cb=20150627163627",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+
+{
+  "name":"Raphael",
+  "photo":"https://vignette.wikia.nocookie.net/parody/images/5/5b/Kevin_the_minions_2015.jpg/revision/latest/scale-to-width-down/456?cb=20150507162315",
+  "scores":[
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+{
+  "name":"Gavin",
+  "photo":"https://vignette.wikia.nocookie.net/parody/images/3/35/Stuart_minions_two_thumbs_up.jpg/revision/latest/scale-to-width-down/513?cb=20151101040700",
+  "scores":[
+      4,
+      4,
+      4,
+      4,
+      4,
+      4,
+      2,
+      5,
+      4,
+      1
+    ]
+}
